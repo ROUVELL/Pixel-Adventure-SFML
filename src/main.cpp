606b565ca53ce45dc5ffd66fe1ea063e8075c4bf
ROUVELL <sheva15032005@gmail.com>
2023-11-27 20:19:02 +0200

    some updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
-#include "SFML/Graphics.hpp"
+#include <fstream>
+#include <string>
 
+#include "SFML/Graphics.hpp"
+#include "json.hpp"
+
+
+std::string ROOT_DIR;
 
 class Game
 {
 public:
-	Game();
+	Game() = default;
 
+	bool init();
 	void run();
 
 private:
@@ -18,16 +25,43 @@ private:
 
 int main()
 {
-	Game().run();
+	Game game;
+
+	if (!game.init())
+		return -1;
+
+	game.run();
 
 	return 0;
 }
 
-Game::Game()
+bool Game::init()
 {
-	window.create(sf::VideoMode{ 1200, 900 }, "", sf::Style::None);
+	std::fstream configFile{ "config.json" };
+
+	if (!configFile.is_open())
+		return false;
+
+	nlohmann::json config = nlohmann::json::parse(configFile);
+
+	if (
+		!config.is_object() ||
+		!config.contains("screen_width") ||
+		!config.contains("screen_height") ||
+		!config.contains("root_dir")
+		)
+		return false;
+
+	configFile.close();
+
+	unsigned screenWidth = static_cast<unsigned>(config.value("screen_width", 1200));
+	unsigned screenHegiht = static_cast<unsigned>(config.value("screen_height", 900));
+
+	window.create(sf::VideoMode{ screenWidth, screenHegiht }, "", sf::Style::None);
 	window.setFramerateLimit(60);
 	window.setVerticalSyncEnabled(true);
+
+	return true;
 }
 
 void Game::run()
